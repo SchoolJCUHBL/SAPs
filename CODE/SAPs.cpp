0d9720359febb83b20857061f700a6fc2c157a2b
Victor van Lange <v.vanlange@gmail.com>
2013-12-02 15:12:11 +0100

    Commented array implementation
    moeten waarschijnlijk vectoren gebruiken.
    beetje ingewikkelder, waarschijnlijk veel handiger.

--- CODE/SAPs.cpp
+++ CODE/SAPs.cpp
@@ -8,6 +8,30 @@ using std::endl;
 #include <CL/cl.hpp>
 #define __NO_STD_VECTOR
 
+/*
+void resetArray (int square, bool grid[][])
+{
+        for (int i = 0; i < square; i++) {
+            for (int j = 0; j < square; j++) {
+               block[i][j] = false;
+            }
+    }
+}
+
+void showArray (bool grid[][], int square)
+{
+    for(int i=0; i<square; i++)    //This loops on the rows.
+	{
+        for(int j=0; j<square; j++) //This loops on the columns
+		{
+			cout << block[i][j]  << "  ";
+		}
+		cout << endl;
+	}
+}
+*/
+
+
 //function Main starts the execution
 int main()
 {
@@ -23,6 +47,12 @@ int main()
     }
 
     cout << "SAP length is: " << SAPlength << endl;
+/*
+    bool grid[SAPlength][SAPlength]; //creates a 9*9 matrix or a 2d array.
+
+    resetArray(grid, SAPlength);
+    showArray(grid, SAPlength)
+*/
     return 0;
 }
 
